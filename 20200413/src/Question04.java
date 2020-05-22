e69a2214fa97eed4ad41fadd4a4baa2eed1de552
slsk3633 <slsk3633@gmail.com>
2020-05-22 16:14:37 +0900

    200522 java quiz4 update

--- 20200413/src/Question04.java
+++ 20200413/src/Question04.java
@@ -1,7 +1,9 @@
 //문제4. 다음 계산 결과를 출력하는 프로그램을 작성하되 덧셈 연산의 횟수를 최소화하여 작성
 public class Question04 {
 	public static void main(String[] args) {
+		
 		int result=3+6;
+		
 		System.out.println("3+6=" +result);
 		
 		result += 9;
